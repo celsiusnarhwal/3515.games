171be9bb1df321da0816479327fee9ab739fdc92
celsius narhwal <70828899+celsiusnarhwal@users.noreply.github.com>
2022-04-15 06:30:38 -0400

    refined board png context manager

--- cogs/chess/helpers.py
+++ cogs/chess/helpers.py
@@ -12,13 +12,8 @@ from svglib.svglib import svg2rlg
 
 @contextmanager
 def get_board_png(board: pychess.Board) -> discord.File:
-    directory = Path(tempfile.mkdtemp())
-
-    with directory:
-        with open("board.svg", "w+") as svg:
-            svg.write(pychess_svg.board(board, size=1800))
+    with tempfile.TemporaryDirectory() as tmp:
+        with Path(tmp):
+            open("board.svg", "w+").write(pychess_svg.board(board, size=1800))
             renderPM.drawToFile(svg2rlg("board.svg"), "board.png", fmt="png")
-
-        yield discord.File(os.path.abspath("board.png"), filename="board.png")
-
-    directory.rmtree()
+            yield discord.File(os.path.abspath("board.png"), filename="board.png")
