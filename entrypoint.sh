dabb478c60aefe0f55032b3d0145e8cbc3196a08
celsius narhwal <celsius@celsiusnarhwal.dev>
2022-08-22 15:38:33 -0400

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$ENTRYPOINT" == "api" ]; then
-  doppler run -- uvicorn api:app --host "0.0.0.0"
+  doppler run -- uvicorn api:app --host "0.0.0.0" --port "$PORT"
 else
   doppler run -- poetry run python3 main.py
 fi
