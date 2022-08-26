a487f644b69e28e18682756521a8efb43f121242
celsius narhwal <celsius@celsiusnarhwal.dev>
2022-08-26 12:09:38 -0400

    restructure settings

---
+++ settings/__init__.py
@@ -0,0 +1,17 @@
+"""
+The settings package enables 3515.games to dynamically load a particular settings configuration based on the
+environment it's running in.
+"""
+from settings.base import *
+
+_environments = {
+    "prod": "settings.envs.prod",
+    "dev": "settings.envs.dev",
+}
+
+_current_environment = os.getenv("DOPPLER_ENVIRONMENT")
+
+if _current_environment in _environments.keys():
+    exec(f"from {_environments[_current_environment]} import *")
+else:
+    raise Exception(f"Unknown environment: {_current_environment}")
---
+++ settings/envs/dev.py
@@ -0,0 +1,47 @@
+"""
+Development settings.
+"""
+
+import os
+
+import discord
+
+import cogs
+
+# Bot Name
+BOT_NAME = "3515.games.dev"
+
+# Application ID
+APP_ID = 960228863986761778
+
+# Owner ID (corresponds to celsiusnarhwal#3515)
+OWNER_ID = 170966436125212673
+
+# Gateway Intents (https://discord.com/developers/docs/topics/gateway#gateway-intents)
+INTENTS = discord.Intents.default() + discord.Intents.members
+
+# Debug Guilds
+DEBUG_GUILDS = []
+
+# Extensions
+EXTENSIONS = []
+
+# Disabled Cogs
+DISABLED_COGS = []
+
+# NLTK Corpora (https://www.nltk.org/book/ch02)
+NLTK_CORPORA = ["averaged_perceptron_tagger"]
+
+# Database Settings
+DATABASE_SETTINGS = {
+    "provider": "sqlite",
+    "filename": "db.sqlite",
+    "create_db": True,
+}
+
+# API Settings
+API_HOST = "127.0.0.1"
+API_PORT = 8080
+
+# Bot Token
+TOKEN = os.getenv("DEV_TOKEN")
