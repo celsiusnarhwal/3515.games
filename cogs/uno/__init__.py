adb99deeb8b74b7176f28f998fb5f908a2a05a39
celsius narhwal <hello@celsiusnarhwal.dev>
2023-01-01 09:17:00 -0500

    update copyright year to 2023

--- cogs/uno/__init__.py
+++ cogs/uno/__init__.py
@@ -1,5 +1,5 @@
 ########################################################################################################################
-#                         Copyright (C) 2022-present celsius narhwal <hello@celsiusnarhwal.dev>                        #
+#                         Copyright (C) 2023-present celsius narhwal <hello@celsiusnarhwal.dev>                        #
 #  This notice may not be altered or removed except by or with the express written permission of the copyright holder. #
 #                                      For more information, see the COPYING file.                                     #
 ########################################################################################################################
