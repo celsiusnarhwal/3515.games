639c9f74a4f7eca3872f61b66ce86416fa7fbcea
celsius narhwal <hello@celsiusnarhwal.dev>
2023-02-13 20:08:06 -0500

    update copyright notices

--- keyboard.py
+++ keyboard.py
@@ -1,3 +1,9 @@
+########################################################################################################################
+#                         Copyright (C) 2023-present celsius narhwal <hello@celsiusnarhwal.dev>                        #
+#  This notice may not be altered or removed except by or with the express written permission of the copyright holder. #
+#                                      For more information, see the COPYING file.                                     #
+########################################################################################################################
+
 """
 A shortcut to certain names from the types and typing modules.
 """
