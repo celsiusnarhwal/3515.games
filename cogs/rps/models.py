4103d531b64531acd50d79dfe3d71e59c1782980
celsius narhwal <hello@celsiusnarhwal.dev>
2023-02-27 22:20:08 -0500

    update rps with attrs

--- cogs/rps/models.py
+++ cogs/rps/models.py
@@ -10,42 +10,31 @@ import os
 import random
 
 import discord
+from attrs import define
 
 import support
 from cogs import rps
-from support import BasePlayer
+from support import BasePlayer, Fields
 
 
+@define(slots=False)
 class RPSGame:
     """
     Represents a Rock-Paper-Scissors match.
     """
 
-    def __init__(self, players: list[RPSPlayer], game_format: str, points_to_win: int):
-        """
-        The constructor for `RPSGame`.
+    players: list[RPSPlayer]
+    game_format: str
+    points_to_win: int
 
-        :param players: A list of the game's players.
-        :param game_format: The format of the game; "Best of Three", "Best of Five", or "Best of Nine".
-        :param points_to_win: The number of points needed to win the game. The game will end as soon as one player
-        reaches this many points.
-        """
-        self.players = players
-        self.points_to_win = points_to_win
-        self.game_format = game_format
-        self.challenger = players[0]
-        self.opponent = players[1]
-        self.current_round = 1
-        self.match_record = [[]]
+    current_round: int = Fields.attr(default=1)
+    match_record: list[list[str]] = Fields.attr(factory=lambda: [[]])
 
-    async def issue_challenge(self, ctx: discord.ApplicationContext):
-        """
-        This function handles the actual background work of issuing a challenge to a user.
+    def __attrs_post_init__(self):
+        self.challenger = self.players[0]
+        self.opponent = self.players[1]
 
-        :param ctx: An ApplicationContext object.
-        :return: True if the challenge recipient accepts the challenge, False otherwise.
-        """
-
+    async def issue_challenge(self, ctx: discord.ApplicationContext) -> bool:
         # confirm with the challenger (i.e. the invoker of /rps challenge) that they want to issue the challenge
         view = support.ConfirmationView(ctx=ctx)
         challenge_confirmation = await view.request_confirmation(
@@ -79,11 +68,6 @@ class RPSGame:
                 return False
 
     async def game_intro(self, ctx):
-        """
-        Announces the start of a Rock-Paper-Scissors match.
-
-        :param ctx: An ApplicationContext object.
-        """
         game_intro_text = (
             f"**Players:** {self.challenger.user.mention} vs."
             f" {self.opponent.user.mention}\n"
@@ -107,24 +91,12 @@ class RPSGame:
 
             await ctx.send(embed=intro_embed, file=gif_file)
 
-    async def select_player_moves(self, ctx):
-        """
-        Prompts both players to select their moves.
-
-        :param ctx: An ApplicationContext object.
-        :return: True if move selection completed successfully, False otherwise.
-        """
+    async def select_player_moves(self, ctx) -> bool:
         return await rps.RPSChooseMoveView(
             ctx=ctx, players=self.players
         ).start_move_selection(round_number=self.current_round)
 
     async def report_round_results(self, ctx):
-        """
-        Announces the results of a round and adds them to the game's match record.
-
-        :param ctx: An ApplicationContext object.
-        """
-
         # dictionary of winning cases for rock-paper-scissors, where they key "beats" the value. rock beats scissors,
         # scissors beats paper, paper beats rock.
         winning_cases = {
@@ -184,24 +156,13 @@ class RPSGame:
         self.challenger.clear_selected_move()
         self.opponent.clear_selected_move()
 
-    def check_for_match_winner(self):
-        """
-        Checks to see whether a player has reached the point threshold for victory. This is called after every round.
-
-        :return: The victorious player if applicable, otherwise None.
-        """
+    def check_for_match_winner(self) -> RPSPlayer | None:
         return next(
             (player for player in self.players if player.score == self.points_to_win),
             None,
         )
 
     async def end_match(self, ctx, winner: RPSPlayer):
-        """
-        Ends a Rock-Paper-Scissors game. This is only called when a player has reached the point threshold for victory.
-
-        :param ctx: An ApplicationContext object.
-        :param winner: The winner of the match.
-        """
         self.match_record[-1].append(f"**Match Winner:** {winner.user.mention}")
 
         await rps.RPSMatchEndView(
@@ -209,19 +170,14 @@ class RPSGame:
         ).show_match_results(players=self.players, winner=winner)
 
 
+@define
 class RPSPlayer(BasePlayer):
     """
     Represents a player in a Rock-Paper-Scissors match.
     """
 
-    def __init__(self, user: discord.User):
-        """
-        The constructor for `RPSPlayer`.
-        :param user: The user to construct a player from.
-        """
-        super().__init__(user)
-        self.selected_move = None
-        self.score = 0
+    selected_move: str = Fields.attr(default=None)
+    score: int = Fields.attr(default=0)
 
     def clear_selected_move(self):
         """
