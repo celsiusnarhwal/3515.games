878b98ba347f46bca7e0b306150debf8c14fee7c
celsius narhwal <hello@celsiusnarhwal.dev>
2023-04-08 12:44:36 -0400

    update copyright notices

--- poet.py
+++ poet.py
@@ -1,3 +1,9 @@
+########################################################################################################################
+#                         Copyright (C) 2023-present celsius narhwal <hello@celsiusnarhwal.dev>                        #
+#  This notice may not be altered or removed except by or with the express written permission of the copyright holder. #
+#                                      For more information, see the COPYING file.                                     #
+########################################################################################################################
+
 """
 Maintains a consistent Poetry version in Docker containers and CI systems.
 """
